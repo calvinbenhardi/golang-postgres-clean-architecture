36e28d22c8639182cc74ffa103e77987d8970407
calvinbenhardi <calvinbenhardi@gmail.com>
2018-11-22 12:53:14 +0700

    Create repository to handle sql action

--- controllers/book.go
+++ controllers/book.go
@@ -2,10 +2,12 @@ package controllers
 
 import (
 	"books-list/models"
+	"books-list/repository/book"
 	"database/sql"
 	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -25,17 +27,9 @@ func (c Controller) GetBooks(db *sql.DB) http.HandlerFunc {
 		var book models.Book
 		books = []models.Book{}
 
-		rows, err := db.Query("select * from books")
-		logFatal(err)
+		bookRepo := bookRepository.BookRepository{}
+		books = bookRepo.GetBooks(db, book, books)
 
-		defer rows.Close()
-
-		for rows.Next() {
-			err := rows.Scan(&book.ID, &book.Title, &book.Author, &book.Year)
-			logFatal(err)
-
-			books = append(books, book)
-		}
 		json.NewEncoder(w).Encode(books)
 	}
 }
@@ -45,10 +39,13 @@ func (c Controller) GetBook(db *sql.DB) http.HandlerFunc {
 		var book models.Book
 		params := mux.Vars(r)
 
-		rows := db.QueryRow("select * from books where id=$1", params["id"])
+		books = []models.Book{}
+		bookRepo := bookRepository.BookRepository{}
 
-		err := rows.Scan(&book.ID, &book.Title, &book.Author, &book.Year)
+		id, err := strconv.Atoi(params["id"])
 		logFatal(err)
+
+		book = bookRepo.GetBook(db, book, id)
 
 		json.NewEncoder(w).Encode(book)
 	}
@@ -60,8 +57,8 @@ func (c Controller) CreateBook(db *sql.DB) http.HandlerFunc {
 
 		json.NewDecoder(r.Body).Decode(&book)
 
-		err := db.QueryRow("insert into books(title, author, year) values($1, $2, $3) RETURNING id;", book.Title, book.Author, book.Year).Scan(&bookID)
-		logFatal(err)
+		bookRepo := bookRepository.BookRepository{}
+		bookID = bookRepo.CreateBook(db, book)
 
 		json.NewEncoder(w).Encode(&bookID)
 	}
@@ -71,11 +68,8 @@ func (c Controller) UpdateBook(db *sql.DB) http.HandlerFunc {
 		var book models.Book
 		json.NewDecoder(r.Body).Decode(&book)
 
-		result, err := db.Exec("update books set title=$1, author=$2, year=$3 where id=$4 RETURNING id", &book.Title, &book.Author, &book.Year, &book.ID)
-		logFatal(err)
-
-		rowsUpdated, err := result.RowsAffected()
-		logFatal(err)
+		bookRepo := bookRepository.BookRepository{}
+		rowsUpdated := bookRepo.UpdateBook(db, book)
 
 		json.NewEncoder(w).Encode(rowsUpdated)
 
@@ -85,13 +79,14 @@ func (c Controller) RemoveBook(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		params := mux.Vars(r)
 
-		delete, err := db.Exec("delete from books where id=$1", params["id"])
+		bookRepo := bookRepository.BookRepository{}
+
+		id, err := strconv.Atoi(params["id"])
 		logFatal(err)
 
-		rowsUpdated, err := delete.RowsAffected()
-		logFatal(err)
+		rowsDeleted := bookRepo.RemoveBook(db, id)
 
-		json.NewEncoder(w).Encode(rowsUpdated)
+		json.NewEncoder(w).Encode(rowsDeleted)
 
 	}
 }
